056453121ed79a70589336021d944e7c380059d2
dolphinman <wpfan.org@gmail.com>
2018-04-24 20:52:16 -0500

    updated and added new folders

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+//Hold a list of questions and create an empty array//
+var questions = [{
+question: "What is your favorite color?",
+answers: ["blue", "green", "purple", "red"],
+correctAnswer: "blue"
+}, 
+{
+question: "What is your favorite day?",
+answers: ["monday", "tuesday", "wednesday", "thursday"],
+correctAnswer: "wednesday"
+
+}];
+
+console.log(questions[0].answers[2]);
+
