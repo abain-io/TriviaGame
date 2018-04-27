4351f71442969f58cb4ffad3b5db2ceaf8dd09ec
dolphinman <wpfan.org@gmail.com>
2018-04-26 21:21:24 -0500

    updated all files with new changes -still working and trying to finish

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,36 @@
 //Hold a list of questions and create an empty array//
-var questions = [{
-question: "What is your favorite color?",
-answers: ["blue", "green", "purple", "red"],
-correctAnswer: "blue"
-}, 
-{
-question: "What is your favorite day?",
-answers: ["monday", "tuesday", "wednesday", "thursday"],
-correctAnswer: "wednesday"
+// //var questions =
+//         question: "What is was the first full length CGI movie?",
+//         answers: ["A Bugs Life", "Monsters Inc", "Toy Story", "The Lion King"],
+//         correctAnswer: "Toy Story"
+//     
+    // 
+    //     question: "Which NBA team won the most titles in the 90's?",
+    //     answers: ["New York Knicks", "Portland Trailblazers", "Los Angeles Lakers", "Chicago Bulls"],
+    //     correctAnswer: "Da Bulls"
+//     
+// 
+// console.log(questions[0].answers[3]);
 
-}];
+var timer;
 
-console.log(questions[0].answers[2]);
+var counter = 10;
+
+function countdown() {
+    counter--
+    $("#counter").html(counter);
+    if (counter === 0) {
+        clearInterval(timer)
+    }
+}
+
+function start() {
+    timer = setInterval(countdown, 1000);
+}
+
+$( "#start" ).click(function() {
+    start();
+  });
+
+//   I can't figure out how to do the rest...//
 
